db115fbe8afd1fde4223deac2efcda1a8d185233
Havok <rngdhavok@hotmail.com>
2023-01-31 02:38:06 -0500

    Added materialize css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 var currentDate = dayjs().format('YYYY-MM-DD');
+var liveScores = document.querySelector(".card-content");
+var odds = document.querySelector("#odds");
 
-
-function getApi(){
+// GET ALL NBA GAMES
+function getGames(){
     fetch ("https://v1.basketball.api-sports.io/games?league=12&season=2022-2023&timezone=America/New_York&date=" + currentDate, {
 	    "method": "GET",
 	    "headers": {
@@ -10,20 +12,40 @@ function getApi(){
 	    }
     })
     .then(response => {
-        console.log(response.json());
+
     return response.json();
     
+    })
+    .then (function(data) {
+        console.log(data);
+
+        for (i=0; i < data.response.length - 1; i++){
+            var span = document.createElement("span");
+            var line = document.createElement("li");
+
+            span.setAttribute("id", "teamnames"+i);
+            liveScores.appendChild(span);
+            var teamNames = document.querySelector("#teamnames"+i)
+
+            line.setAttribute("id", "game"+i);
+            liveScores.appendChild(line);
+            var gameScores = document.querySelector("#game"+i);
+
+
+            teamNames.textContent = data.response[i].teams.home.name + "  VS.  " + data.response[i].teams.away.name;
+            gameScores.textContent = data.response[i].scores.away.total + " - " + data.response[i].scores.home.total;
+        }
+
     })
     .catch(err => {
 	console.log(err);
     });
+
+    
+    
 }
 
-getApi();
-
-
-
-
+// getGames();
 
 var teamName;
 
@@ -31,10 +53,14 @@ function getGifApi(){
     fetch ("https://api.giphy.com/v1/gifs/search?api_key=F6a8XYTRkUMdxsTcwsoz13tuoKcObSik&q="+ teamName +"&limit=25&offset=0&rating=pg-13&lang=en")
     
         .then(response => {
-            console.log(response.json());
+            
             return response.json();
         	
             })
+        .then(data => {
+            console.log(data);
+
+        })
         .catch(err => {
         	console.log(err);
             });
